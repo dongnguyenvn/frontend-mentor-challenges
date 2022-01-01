d80f2a96a3d9826a1a94f5a087534068931c9586
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2022-01-02 04:53:06 +0700

    theme toggle

--- react-calculator-app/src/App.tsx
+++ react-calculator-app/src/App.tsx
@@ -1,12 +1,20 @@
+import clsx from 'clsx'
 import './App.css'
 import { Calculator, ThemeToggle } from './components'
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <main>
-        <header>
-          <h1 className='text-3xl font-bold'>Calc</h1>
+    <div
+      className={clsx(
+        'w-screen h-screen px-6 py-8 flex justify-center items-center',
+        'blue:bg-blue-500 blue:text-white',
+        'cyan:bg-gray-500 cyan:text-yellow-700',
+        'violet:bg-violet-900 violet:text-yellow-400',
+      )}
+    >
+      <main className="flex flex-col gap-8 w-screen max-w-xl">
+        <header className={clsx('flex justify-between items-end')}>
+          <h1 className="text-3xl font-bold">Calc</h1>
           <ThemeToggle />
         </header>
         <Calculator />
--- react-calculator-app/src/components/ThemeToggle.tsx
+++ react-calculator-app/src/components/ThemeToggle.tsx
@@ -1,4 +1,11 @@
-const themes = [
+import clsx from 'clsx'
+import { ChangeEvent, useEffect, useState } from 'react'
+
+type Theme = {
+  label: string
+  value: string
+}
+const themes: Theme[] = [
   {
     label: '1',
     value: 'theme-blue',
@@ -13,8 +20,87 @@ const themes = [
   },
 ]
 
-const ThemeToggle = () => {
-  return <div>theme toggle</div>
+const Toggle = ({ index }: { index: number }) => {
+  return (
+    <div
+      className={clsx(
+        'absolute rounded-full w-4 h-4 pointer-events-none',
+        'transition duration-150',
+        'blue:bg-red-500',
+        'cyan:bg-orange-700',
+        'violet:bg-cyan-500',
+      )}
+      style={{
+        transform: `translateX(${index * 100}%)`,
+        margin: `${index * 0.25}rem`,
+      }}
+    />
+  )
 }
 
-export {ThemeToggle}
+const ThemeToggle = () => {
+  const [active, setActive] = useState('theme-blue')
+
+  useEffect(() => {
+    themes.forEach(({ value }) =>
+      document.body.classList.toggle(value, value === active),
+    )
+  }, [active])
+
+  const index = themes.findIndex(({ value }) => value === active)
+  const handleChangeTheme = (e : ChangeEvent<HTMLInputElement>) => {
+    setActive(e.target.value)
+  }
+  return (
+    <div
+      role="group"
+      aria-labelledby="theme"
+      className="flex items-end gap-4"
+      onChangeCapture={handleChangeTheme}
+    >
+      <span id="theme" className="font-bold text-xs pb-0.5">
+        THEME
+      </span>
+
+      <div>
+        <div className="flex px-1.5 justify-around gap-1">
+          {themes.map(({ label, value }) => (
+            <label
+              key={value}
+              htmlFor={value}
+              className="mb-0.5 font-bold text-xs"
+            >
+              {label}
+            </label>
+          ))}
+        </div>
+
+        <div
+          className={clsx(
+            'flex items-center rounded-full p-1 gap-1 relative',
+            'blue:bg-blue-700',
+            'cyan:bg-red-700',
+            'violet:bg-violet-800',
+          )}
+        >
+          {themes.map(({ value }) => (
+            <div key={value} className="w-4 flex justify-center items-center">
+              <input
+                type="radio"
+                name="theme"
+                id={value}
+                value={value}
+                defaultChecked={value === active}
+                className="opacity-0"
+              />
+            </div>
+          ))}
+
+          <Toggle index={index} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export { ThemeToggle }
