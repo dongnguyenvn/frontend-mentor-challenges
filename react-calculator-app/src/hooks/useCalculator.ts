b3fd50f2f9dad1af7345d64bcb6d2f819a1ee916
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2022-01-04 07:22:09 +0700

    fix reducer

--- react-calculator-app/src/hooks/useCalculator.ts
+++ react-calculator-app/src/hooks/useCalculator.ts
@@ -1,107 +1,99 @@
 import {
-    findLast,
-    isOperator,
-    findLastOperator,
-    calculate,
-    format,
-  } from "../libs/calculator";
-  import { useReducer, useMemo } from "react";
-  
-  const initialState = ["0"];
+  findLast,
+  isOperator,
+  findLastOperator,
+  calculate,
+  format,
+} from '../libs/calculator'
+import { useReducer, useMemo, Reducer } from 'react'
 
+const initialState = ['0']
 
-  enum CountActionKind {
-    RESET = 'RESET',
-    PUSH = 'PUSH',
-    REMOVE = 'REMOVE',
-    OPERATE = 'OPERATE',
-    ENTER = 'ENTER',
+enum CountActionKind {
+  RESET,
+  PUSH,
+  REMOVE,
+  OPERATE,
+  ENTER,
+}
+
+type Action = {
+  type: CountActionKind
+  value ?: any
+}
+
+const calculatorReducer: Reducer<string[], Action> = (state = [], action) => {
+  if (action.type === CountActionKind.RESET) {
+    return initialState
   }
 
-  type Action = {
-    type: CountActionKind,
-    value?: string
+  if (action.type === CountActionKind.PUSH) {
+    const last = findLast(state)
+
+    if (isOperator(last)) {
+      return [...state, action.value]
+    }
+
+    if (action.value === '.' && last.includes(action.value)) {
+      return state
+    }
+
+    if (action.value === '.') {
+      return [...state.slice(0, -1), last + action.value]
+    }
+
+    return [...state.slice(0, -1), last + action.value]
   }
-  
-  function reducer(state:string[], action : Action){
-    if (action.type === "RESET") {
-      return initialState;
+
+  if (action.type === CountActionKind.REMOVE) {
+    const last = findLast(state)
+
+    if (isOperator(last)) {
+      return state.slice(0, -1)
     }
-  
-    if (action.type === CountActionKind.PUSH) {
-      const last = findLast(state);
-  
-      if (isOperator(last)) {
-        return [...state, action.value];
-      }
-  
-      if (action.value === "." && last.includes(action.value)) {
-        return state;
-      }
-  
-      if (action.value === ".") {
-        return [...state.slice(0, -1), last + action.value];
-      }
-  
-      return [...state.slice(0, -1), last + action.value];
-    }
-  
-    if (action.type === CountActionKind.REMOVE) {
-      const last = findLast(state);
-  
-      if (isOperator(last)) {
-        return state.slice(0, -1);
-      }
-  
-      return [...state.slice(0, -1), String(last).slice(0, -1)];
-    }
-  
-    if (action.type === CountActionKind.OPERATE) {
-      const last = findLast(state);
-  
-      if (isOperator(last)) {
-        return [...state.slice(0, -1), action.value];
-      }
-  
-      if (findLastOperator(state) === action.value) {
-        return [...calculate(state), action.value];
-      }
-  
-      return [...state, action.value];
-    }
-  
-    if (action.type === CountActionKind.ENTER) {
-      return calculate(state);
-    }
-  
-    return state;
+
+    return [...state.slice(0, -1), String(last).slice(0, -1)]
   }
-  
-  export function useCalculator() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-  
-    const actions = useMemo(
-        const action : {
-            reset : () => () => void,
-            push : (value: string) => () => void,
-          remove : () => () => void,
-          operate : (value: string) => () => void,
-          enter :() => () => void
-        } = {
-            reset: () => () => dispatch({ type: CountActionKind.RESET }),
-            push: (value:string) => () => dispatch({ type: PUSH, value }),
-            remove: () => () => dispatch({ type: REMOVE }),
-            operate: (value:string) => () => dispatch({ type: OPERATE, value }),
-            enter: () => () => dispatch({ type: ENTER }),
-          }
-      () => (action),
-      [dispatch]
-    );
-  
-    //  compute
-    const operator = findLastOperator(state);
-    const last = findLast(state);
-    const output = format(isOperator(last) ? state[state.length - 2] : last);
-  
-    return { operator, output, actions };
+
+  if (action.type === CountActionKind.OPERATE) {
+    const last = findLast(state)
+
+    if (isOperator(last)) {
+      return [...state.slice(0, -1), action.value]
+    }
+
+    if (findLastOperator(state) === action.value) {
+      return [...calculate(state), action.value]
+    }
+
+    return [...state, action.value]
   }
+
+  if (action.type === CountActionKind.ENTER) {
+    return calculate(state)
+  }
+
+  return state
+}
+
+export function useCalculator() {
+  const [state, dispatch] = useReducer(calculatorReducer, initialState)
+
+  const actions = useMemo(
+    () => ({
+      reset: () => () => dispatch({ type: CountActionKind.RESET }),
+      push: (value: string) => () => dispatch({ type: CountActionKind.PUSH, value }),
+      remove: () => () => dispatch({ type: CountActionKind.REMOVE }),
+      operate: (value: string) => () => dispatch({ type: CountActionKind.OPERATE, value }),
+      enter: () => () => dispatch({ type: CountActionKind.ENTER }),
+    }),
+    [dispatch],
+  )
+
+  //  compute
+  const operator = findLastOperator(state)
+  const last = findLast(state)
+  const output = format(isOperator(last) ? state[state.length - 2] : last)
+
+  return { operator, output, actions }
+}
